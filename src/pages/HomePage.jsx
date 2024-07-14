22e4e9c4209df058e1c9145d3233559fc149106f
Bashari Rivka <basrivka1@gmail.com>
2024-07-14 14:48:16 +0300

    Add home and aboutUs pages

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+
+
+
+export function HomePage() {
+
+return (
+   <h1>Hello to MISTER TOY</h1> 
+)
+
+}
--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,2 +1,34 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
+
+import { AppHeader } from './cmps/AppHeader.jsx'
+
+import { HomePage } from './pages/HomePage.jsx'
+import { AboutUs } from './pages/AboutUs.jsx'
+import { ToyIndex } from './pages/ToyIndex.jsx'
+import { store } from './store/store.js'
+
+import './assets/style/main.css'
+
+export function App() {
+
+    return (
+        <Provider store={store}>
+            <Router>
+                <section className="app">
+                    <AppHeader />
+                    <main className='main-layout'>
+                        <Routes>
+                            <Route element={<HomePage />} path="/" />
+                            <Route element={<AboutUs />} path="/about" />
+                            <Route element={<ToyIndex />} path="/Toy" />
+                        </Routes>
+                    </main>
+                    <AppFooter />
+                </section>
+            </Router>
+        </Provider>
+    )
+}
+
+
